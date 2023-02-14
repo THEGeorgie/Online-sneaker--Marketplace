e28d0140a2356c104406ae89583a764c7c9e58c0
Gogi Danev <gorgodanev@gmail.com>
2023-02-14 17:31:57 +0100

    Commit

--- save_post.php
+++ save_post.php
@@ -2,7 +2,6 @@
     session_start();
 
     require_once 'connection.php';
-
     $name = $_SESSION['name'];
 
     $query = "SELECT is_seler AND COUNT(*) as count FROM `member` WHERE `username` = :name";
