a4b0e5c52251aa9f6fb03845860754e54d030d49
Gogi Danev <gorgodanev@gmail.com>
2023-02-14 18:41:17 +0100

    comm

--- save_post.php
+++ save_post.php
@@ -3,7 +3,6 @@
 
     require_once 'connection.php';
     $name = $_SESSION['name'];
-
     $query = "SELECT is_seler AND COUNT(*) as count FROM `member` WHERE `username` = :name";
     $stmt->bindParam(':name', $name);
     $stmt->execute();
