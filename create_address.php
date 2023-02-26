f82cfcfa036813203a816c856b62a4a4cd1af7ad
Gogi Danev <gorgodanev@gmail.com>
2023-02-26 14:54:52 +0100

    Shit iant working

--- create_address.php
+++ create_address.php
@@ -1,5 +1,61 @@
 <?php
-    require_once('connection.php');
+    session_start();
+    require_once('connection.php'); 
 
-    
+    if(isset($_POST['uplaodAddress'])){
+        $ime = $_POST['name'];
+        $primek = $_POST['surname'];
+        $address1 = $_POST['address1'];
+        $address2 = $_POST['address2'];
+        $mesto = $_POST['city'];
+        $drzava = $_POST['country'];
+        $postnaStevilka = $_POST['postalCode'];
+        $mobilanStevilka = $_POST['phoneNumber'];
+        $Aid = $_SESSION['id'];
+
+        if ($_SESSION['seller'] == 2) {
+            $querryP = "INSERT INTO 'naslov_za_posiljanje_prodajalec' (adres1, adres2, postna_stevilka, telefonska_stevilka prod_id, Ime, Primek, drzava, mesto) VALUES (:address1, :address2, :postnaStevilka, :mobilanStevilka, :Aid, :ime, :primek, :drzava, :mesto)";
+            $stmt_adresP = $conn->prepare($querryP);
+            $stmt_adresP->bindParam(':address1', $address1);
+            $stmt_adresP->bindParam(':address2', $address2);
+            $stmt_adresP->bindParam(':postnaStevilka', $postnaStevilka);
+            $stmt_adresP->bindParam(':mobilanStevilka', $mobilanStevilka);
+            $stmt_adresP->bindParam(':Aid', $Aid);
+            $stmt_adresP->bindParam(':ime', $ime);
+            $stmt_adresP->bindParam(':primek', $primek);
+            $stmt_adresP->bindParam(':drzava', $drzava);
+            $stmt_adresP->bindParam(':mesto', $mesto);
+            if ($stmt_adresP->execute()) {
+                $_SESSION['success'] = "Adress saved successfully";
+                header('location:profile.php');
+            }else{
+                $_SESSION['error'] = "Something went wrong......";
+                header('location:profile.php');
+            }
+        }else {
+            $querryS = "INSERT INTO 'Naslov_za_posiljanje_stranka' (adres1, adres2, postna_stevilka, telefonska_stevilka strank_id, Ime, Primek, drzava, mesto) 
+            VALUES (:address1, :address2, :postnaStevilka, :mobilanStevilka, :Aid, :ime, :primek, :drzava, :mesto)";
+            $stmt_adresS = $conn->prepare($querryS);
+            $stmt_adresS->bindParam(':address1', $address1);
+            $stmt_adresS->bindParam(':address2', $address2);
+            $stmt_adresS->bindParam(':postnaStevilka', $postnaStevilka);
+            $stmt_adresS->bindParam(':mobilanStevilka', $mobilanStevilka);
+            $stmt_adresS->bindParam(':Aid', $Aid);
+            $stmt_adresS->bindParam(':ime', $ime);
+            $stmt_adresS->bindParam(':primek', $primek);
+            $stmt_adresS->bindParam(':drzava', $drzava);
+            $stmt_adresS->bindParam(':mesto', $mesto);
+            if ($stmt_adresS->execute()) {
+                $_SESSION['success'] = "Adress saved successfully";
+                header('location: profile.php');
+            }else{
+                $_SESSION['error'] = "Something went wrong......";
+                header('location: profile.php');
+            }
+
+        }
+
+        
+    }
 ?>
+
--- profile.php
+++ profile.php
@@ -6,7 +6,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="js/app.js"></script>
+	<script src="assets/js/app.js"></script>
 	<link rel="stylesheet" href="assets/css/style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -60,15 +60,87 @@
 					</div>
 					<div class="col-sm-12 col-lg-6">
 						<h3>Address</h3>
-						<a class="btn btn-outline-light" href="/?ad=address"> Add adress</a>
-						<?php
-							if (isset($_GET['ad'])) {
-								if ($_GET['ad'] == 'address') {
-									include_once('address.php');
-								}
-							}
-						?>
-						
+						<p>
+							<button class="btn btn-outline-light" type="button" data-bs-toggle="collapse"
+								data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+								【Ａｄｄ　ａｄｄｒｅｓｓ】
+							</button>
+						</p>
+						<div class="collapse" id="collapseExample">
+							<form method="POST" action="create_address.php">
+								<div class="row">
+									<div class="col-6">
+										<div class="bg-transparent form-floating mt-3">
+											<input type="text" name="name" class="form-control taxt-dark" id="name"
+												required="required" placeholder="Name">
+											<label for="floatingInput" class="text-dark">Name</label>
+											<ul class="list-group"></ul>
+										</div>
+									</div>
+									<div class="col-6">
+										<div class="bg-transparent form-floating mt-3">
+											<input type="text" name="surname" class="form-control" id="surname"
+												required="required" placeholder="Name">
+											<label for="floatingInput" class="text-dark">Surname</label>
+											<ul class="list-group"></ul>
+										</div>
+									</div>
+									<div class="col-12">
+										<div class="bg-transparent form-floating mt-3">
+											<input type="text" name="address1" class="form-control" id="Address1"
+												required="required" placeholder="Name">
+											<label for="floatingInput" class="text-dark">Address1</label>
+											<ul class="list-group"></ul>
+										</div>
+									</div>
+									<div class="col-12">
+										<div class="bg-transparent form-floating mt-3">
+											<input type="text" name="address2" class="form-control" id="Address2"
+												placeholder="Name">
+											<label for="floatingInput" class="text-dark">Address 2 (optional)</label>
+											<ul class="list-group"></ul>
+										</div>
+									</div>
+									<div class="col-8">
+										<div class="bg-transparent form-floating mt-3">
+											<input type="text" name="city" class="form-control" id="City"
+												placeholder="Name">
+											<label for="floatingInput" class="text-dark">City</label>
+											<ul class="list-group"></ul>
+										</div>
+									</div>
+									<div class="col-4">
+										<div class="bg-transparent form-floating mt-3">
+											<input type="text" name="country" class="form-control" id="City"
+												placeholder="Name">
+											<label for="floatingInput" class="text-dark">Country</label>
+											<ul class="list-group"></ul>
+										</div>
+									</div>
+									<div class="col-4">
+										<div class="bg-transparent form-floating mt-3">
+											<input type="text" name="postalCode" class="form-control" id="PostalCode"
+												placeholder="Name">
+											<label for="floatingInput" class="text-dark">Postal code</label>
+											<ul class="list-group"></ul>
+										</div>
+									</div>
+									<div class="col-8">
+										<div class="bg-transparent form-floating mt-3">
+											<input type="text" name="phoneNumber" class="form-control" id="phoneNumber"
+												placeholder="Name">
+											<label for="floatingInput" class="text-dark">Phone number</label>
+											<ul class="list-group"></ul>
+										</div>
+									</div>
+									<div class="col-12 text-center">
+										<input type="submit" name="uplaodAddress" value="【Ｓｕｂｍｉｔ】"
+											class="btn btn-outline-light btn-block my-3">
+									</div>
+								</div>
+							</form>
+						</div>
+
 					</div>
 				</div>
 			</div>
